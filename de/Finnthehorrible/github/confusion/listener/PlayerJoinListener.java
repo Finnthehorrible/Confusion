c7ab8a290bdaf02291b60c130da7cd8b903da9df
Finnthehorrible <isi220671@onlinehome.de>
2016-01-23 12:13:47 +0100

    More Work

---
+++ de/Finnthehorrible/github/confusion/listener/PlayerJoinListener.java
@@ -0,0 +1,35 @@
+package de.Finnthehorrible.github.confusion.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+        if ( p.getName().equalsIgnoreCase(ChatColor.DARK_RED + "justSanjo") ) {
+            if ( p.getName().equalsIgnoreCase(ChatColor.RED + "Komp3tenz") ) {
+                if ( p.getName().equalsIgnoreCase(ChatColor.RED + "LogischerTundra") ) {
+                    if ( p.getName().equalsIgnoreCase(ChatColor.DARK_BLUE + "Finnthehorrible") ) {
+                        if ( p.getName().equalsIgnoreCase(ChatColor.DARK_BLUE + "Finnthehorrible") ) {
+                            if ( p.getName().equalsIgnoreCase(ChatColor.BLUE + "Buchstabensalat") ) {
+                                if ( p.getName().equalsIgnoreCase(ChatColor.BLUE + "NinjaToni") ) {
+                                    if ( p.getName().equalsIgnoreCase(ChatColor.YELLOW + "End3rflash") ) {
+                                        if ( p.getName().equalsIgnoreCase(ChatColor.YELLOW + "GinDraco") ) {
+                                            if ( p.getName().equalsIgnoreCase(ChatColor.YELLOW + "1000aqua") ) {
+                                            }
+                                            event.setJoinMessage(ChatColor.GREEN + p.getName() + "ist dem Spiel beigetreten!");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
